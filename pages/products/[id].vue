a6840df6d192c12171713b4140e28b3d8dd484b2
dfirgers <dfirger@gmail.com>
2023-02-25 19:15:14 +0200

    add navigation

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>product details for {{ id }}</p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto eos totam
+      repudiandae corrupti soluta quisquam, laborum sequi ratione iste
+      dignissimos eum maiores nobis vel reiciendis. Sapiente vel amet id atque.
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
