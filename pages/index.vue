b47182b01cc32d5f7fc00fc893c9e97f4c004196
dfirgers <dfirger@gmail.com>
2023-02-25 18:16:27 +0200

    add pages folder

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+      aperiam deleniti nulla, minus inventore labore mollitia ab rerum. Maxime
+      maiores fuga libero sunt debitis laboriosam minima, tempora consequatur
+      sequi veniam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+      aperiam deleniti nulla, minus inventore labore mollitia ab rerum. Maxime
+      maiores fuga libero sunt debitis laboriosam minima, tempora consequatur
+      sequi veniam!
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 30px 0px;
+}
+</style>
