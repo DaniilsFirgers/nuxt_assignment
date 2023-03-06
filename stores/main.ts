cac08be5d93b9e70a901cefabf72d064d7c8f2d8
dfirgers <dfirger@gmail.com>
2023-03-06 20:38:03 +0200

    change logic in pinia

--- stores/main.ts
+++ stores/main.ts
@@ -1,42 +1,62 @@
 import { defineStore } from "pinia";
-import axios from "axios";
 
-export const useTaskStore = defineStore("taskStore", {
-  state: () => ({
-    tasks: [] as any[],
-    isLoading: false,
-    isDisabled: false,
-  }),
-  getters: {
-    getUrgent: (state) =>
-      state.tasks.filter((task) => task.urgency === "urgent"),
-  },
-  actions: {
-    async fetchTodos() {
-      this.isLoading = true;
-      try {
-        const res1 = await useFetch("/api/todos");
-        console.log("res", res1.data.value);
 
-        this.tasks = res1.data.value;
-      } catch (err) {
-        console.log("error", err);
-      } finally {
-        this.isLoading = false;
-      }
+type TaskStore = {
+  tasks: any[] | null | { [key: string]: string };
+  isLoading: boolean;
+  isDisabled: boolean;
+};
+type Getters = {
+  getUrgent: (state: any) => { todo: string; urgency: string }[];
+};
+type Actions = {
+  fetchTodos: () => Promise<void>;
+  addTodo: (task: any) => Promise<void>;
+};
+export const useTaskStore = defineStore<string, TaskStore, Getters, Actions>(
+  "taskStore",
+  {
+    state: () => ({
+      tasks: [],
+      isLoading: false,
+      isDisabled: false,
+    }),
+    getters: {
+      getUrgent: (state) =>
+        (state.tasks as { todo: string; urgency: string }[]).filter(
+          (task) => task.urgency === "urgent"
+        ),
     },
-    async addTodo(task: any) {
-      this.isDisabled = true;
-      this.isLoading = true;
-      try {
-        await axios.post("api/todos", task);
-        await this.fetchTodos();
-      } catch (err) {
-        console.log(err);
-      } finally {
-        this.isDisabled = false;
-        this.isLoading = false;
-      }
+    actions: {
+      async fetchTodos() {
+        this.isLoading = true;
+        try {
+          const res = await useFetch("/api/todos");
+          this.tasks = res.data.value;
+        } catch (err) {
+          console.log("error", err);
+        } finally {
+          this.isLoading = false;
+        }
+      },
+      async addTodo(task: any) {
+        this.isDisabled = true;
+        this.isLoading = true;
+        try {
+          await useFetch("api/todos", {
+            headers: { "Content-type": "application/json" },
+            method: "POST",
+            body: task,
+          });
+
+          await this.fetchTodos();
+        } catch (err) {
+          console.log(err);
+        } finally {
+          this.isDisabled = false;
+          this.isLoading = false;
+        }
+      },
     },
-  },
-});
+  }
+);
